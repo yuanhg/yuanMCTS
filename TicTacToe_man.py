d99a6febb050a38460711baa9d89c70e121168e5
yuanhg <faquir.yuen@outlook.com>
2019-04-29 18:08:21 +0800

    2019.4.29

---
+++ TicTacToe_man.py
@@ -0,0 +1,35 @@
+# Author Yuan Honggang
+# Copyright 1.0
+
+from board import Board
+
+xo = Board(3,3,3)
+player = 'x'
+
+while True :
+    xo.oxshow()
+        
+    print('请选手(%s)选择落子处的数字：'%(player), end='')
+    xy = input()
+    playlocation = (int(xy[0])-1, int(xy[1])-1 )
+    
+    if xo.update(player, playlocation ):
+        if xo.oxstates() == 1:
+            print('选手X 赢')
+            xo.oxshow()
+            break
+        elif xo.oxstates() == -1:
+            print('选手O 赢')
+            xo.oxshow()
+            break
+        elif xo.oxstates() == 2:
+            print('平局')
+            xo.oxshow()
+            break
+        elif xo.oxstates() == 0:
+            player = 'o' if player == 'x' else 'x'
+            
+    
+
+
+    
--- board.py
+++ board.py
@@ -1,20 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
 import numpy as np
 
 class Board(object):
-    '''棋盘，默认是“井字棋”TieTacToe，也可以作为五子棋'''
+    '''棋盘，默认是“井字棋”Tie Tac Toe，也可以作为五子棋的棋盘'''
     def __init__(self, height=3, width=3, ninline=3):
         self.width = width     #列
         self.height = height   #行 
+        
         ''' # 表示几个相同的棋子连成一线算作胜利'''
         self.ninline = ninline
         
         ''' # M x N 矩阵，值是棋子类型，0空，1代表'x'，-1代表'o'; ''' 
         self.ox = np.zeros((self.height, self.width), dtype=int )
-        '''棋盘棋局胜负标志，1代表'x'赢，-1代表'o'赢，0代表平局'''
+        
+        '''棋盘棋局胜负标志，1代表'x'赢，-1代表'o'赢，2代表平局，0代表未结束'''
         self.ox_states = 0
-        '''可下位置列表元素如[1:(0,0), 2:(0,1), ]'''
-        self.ox_available = dict(zip( [(k+1) for k in range(self.width*self.height)], 
-                                  [(i+1,j+1) for i in range(self.height) for j in range(self.width)] ))
+        
+        '''现棋盘空余可下位置集合如{(0,0), (0,1), }'''
+        self.ox_available = set( [(i,j) for i in range(self.height) for j in range(self.width)] )
         
     '''显示棋盘，参数是棋盘矩阵'''        
     def oxshow(self):
@@ -22,7 +30,7 @@ class Board(object):
         print('    ', end='') # 5个空格
         for j in range(self.width):
             if j <10:
-                print('  %d '%(j+1), end='')
+                print('   %d '%(j+1), end='')
             elif j <100:
                 print('  %d'%(j+1), end='')
         print('')  # 换行
@@ -46,7 +54,7 @@ class Board(object):
                 elif self.ox[i][j] == 1:   # x 落子
                     print(' X  ', end='')
                 elif self.ox[i][j] == -1:  # o 落子
-                    print(' Q  ', end='') 
+                    print(' O  ', end='') 
             print('')
             '''打印分割线'''
             print('    ', end='')# 5个空格
@@ -55,7 +63,7 @@ class Board(object):
             print('')                    
                 
  
-    '''计算一个ninlin方阵连线情况,1代表'x'连成一线，-1代表'o'，0代表没有'''
+    '''计算一个ninline方阵连线情况,1代表'x'连成一线，-1代表'o'，0代表没有'''
     def nmatrixwinner(self,nm):
         '''把各行列的和组成列表'''
         ox_line = list(nm.sum(axis=1)) + list(nm.sum(axis=0))
@@ -67,7 +75,7 @@ class Board(object):
         return -1 if -nm.shape[0] in ox_line else 1 if nm.shape[0] in ox_line else 0
     
     
-    '''检查棋盘棋局胜负，1代表'x'赢，-1代表'o'赢，0代表平局'''
+    '''检查棋盘棋局胜负，1代表'x'赢，-1代表'o'赢，2代表平局，0代表未结束'''
     def oxwinner(self):
         '''生成棋盘所有ninline维方阵，并判断方阵是否有连成一线的情况'''
         matrixlist = [self.ox[i:i+self.ninline, j:j+self.ninline] 
@@ -80,15 +88,18 @@ class Board(object):
             if self.nmatrixwinner(nm) != 0:
                 return self.nmatrixwinner(nm) 
         
-        '''若所有ninline维方阵返回都是0，则返回0平局'''
-        return 0       
+        '''若所有ninline维方阵返回都是0，且已经没有可落子处，则返回2代表平局，0代表未结束'''
+        if not self.ox_available:
+            return 2
+        else:
+            return 0       
       
     
     '''# player在location(x,y)处落子，更新棋盘，,更新可用位置，计算此时棋局胜负'''
     def update(self, player, location): 
-        if location in self.ox_available.values():
-            self.ox[location[0]-1, location[1]-1] = 1 if player=='x' else -1
-            self.ox_available.pop( (location[0]-1)*self.width + location[1] )    
+        if location in self.ox_available:
+            self.ox[location[0], location[1]] = 1 if player=='x' else -1
+            self.ox_available.remove( location )    
             self.ox_states = self.oxwinner()
             return 1
         else:
@@ -100,8 +111,21 @@ class Board(object):
         return self.ox_states
     
     
-    '''为AI返回一个可以选择落子的字典集合'''
+    '''为AI返回一个可以选择落子的集合'''
     def oxavailable(self):
         return self.ox_available
     
+    
+
+
+# In[2]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
